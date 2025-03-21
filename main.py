93251ea85c4c53de1d88da24601c9a4a4aba5bfd
eugenBod <eugenybod@gmail.com>
2025-03-21 12:27:28 +0300

    Python password verification (ver. 1.1)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 user_password = "qwerty123456"
+flag = True
 
-while True:
+while flag:
     user_input = input("Введите пароль: ")
     if user_input == user_password:
         print("Верный пароль.")
-        break
+        flag = False
     else:
         print("Неверный пароль.")
